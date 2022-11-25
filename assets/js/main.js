aa38843cd182390d652a0b934296458c44bb5750
Madaminov <muhammadanasxon6694@gmail.com>
2022-11-25 18:59:57 +0500

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -126,6 +126,8 @@ sr.reveal(`.home__data, .home__social-link, .home__info,
            .place__card,
            .sponsor__content,
            .footer__data, .footer__rights`,{
+
+            
     origin: 'top',
     interval: 100,
 })
